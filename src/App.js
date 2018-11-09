8f89765980b0e36a1e8e1be6b660659858eda5f5
Varun <svarun0905@gmail.com>
2018-11-09 12:44:06 -0800

    CSS3 Batman

--- src/App.js
+++ src/App.js
@@ -8,20 +8,13 @@ class App extends Component {
       <div className="App">
         <div className="mainBox">
           <div className="boxedDiv">
-                <div className="smallCurveTop leftEar">
-                </div>
-                <div className="bathead">
-                </div>
-                <div className="smallCurveTop rightEar">
-                </div>
-                <div className="bottomLeftCurve1">
-                </div>
-                <div className="bottomLeftCurve2">
-                </div>
-                <div className="bottomLeftCurve3">
-                </div>
-                <div className="bottomLeftCurve4">
-                </div>
+                <div className="smallCurveTop leftEar"></div>
+                <div className="bathead"></div>
+                <div className="smallCurveTop rightEar"></div>
+                <div className="bottomLeftCurve1"></div>
+                <div className="bottomLeftCurve2"></div>
+                <div className="bottomLeftCurve3"></div>
+                <div className="bottomLeftCurve4"></div>
           </div>
         </div>
       </div>
